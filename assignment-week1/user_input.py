afe642380237777d62380225558c2cb9a8004e65
Ezby02 <esperancemwanza@gmail.com>
2024-03-02 14:24:25 +0300

    yay

---
+++ assignment-week1/user_input.py
@@ -0,0 +1,7 @@
+#Program asking user for their name, age and location then prints out a personalised message
+name = input("Enter your name:")
+age = input("Enter your age:")
+location = input("Enter your location:")
+
+print("Hello " + name, "of age " + age, "from " + location, ".")
+print("Welcome!")
